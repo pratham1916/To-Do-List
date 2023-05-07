45c19868f39b3be55a33082f82fa7883cd50b80e
pratham1916 <120631770+pratham1916@users.noreply.github.com>
2023-05-07 11:38:17 +0530

    To Do App

---
+++ main.js
@@ -0,0 +1,70 @@
+
+
+window.addEventListener('load',() => {
+    const form = document.querySelector('#task-form');
+    const input = document.querySelector('#task-input');
+    const list_ele = document.querySelector('#tasks');
+
+    form.addEventListener('submit',(e) => {
+        e.preventDefault();
+
+        const task = input.value;
+
+        if(!task){
+            alert("Add the Tasks");
+            return;
+        }
+        const task_e1 = document.createElement("div");
+        task_e1.classList.add("task");
+
+        const task_content_e1 = document.createElement('div');
+        task_content_e1.classList.add("content");
+
+        task_e1.appendChild(task_content_e1);
+
+        const task_input_e1 = document.createElement("input");
+        task_input_e1.classList.add("text");
+        task_input_e1.type = "text";
+        task_input_e1.value = task;
+        task_input_e1.setAttribute("readonly","readonly");
+
+        task_content_e1.appendChild(task_input_e1);
+
+        const task_action_e1 = document.createElement("div");
+        task_action_e1.classList.add("actions");
+
+        const task_edit_e1 = document.createElement("button");
+        task_edit_e1.classList.add("edit");
+        task_edit_e1.innerHTML = "Edit"
+
+        const task_delete_e1 = document.createElement("button");
+        task_edit_e1.classList.add("delete");
+        task_delete_e1.innerHTML = "Delete";
+
+        task_action_e1.appendChild(task_edit_e1);
+        task_action_e1.appendChild(task_delete_e1);
+
+        task_e1.appendChild(task_action_e1);
+
+        list_ele.appendChild(task_e1);
+
+        input.value = "";
+        
+        task_edit_e1.addEventListener('click', () => {
+
+            if(task_edit_e1.innerText.toLowerCase()=="edit"){
+                task_input_e1.removeAttribute("readonly");
+                task_input_e1.focus();
+                task_edit_e1.innerText = "Save";
+            }
+            else{
+                task_input_e1.setAttribute("readonly","readonly");
+                task_edit_e1.innerText = "Edit";
+            }
+        });
+
+        task_delete_e1.addEventListener('click',() => {
+            list_ele.removeChild(task_e1);
+        });
+    });
+});
